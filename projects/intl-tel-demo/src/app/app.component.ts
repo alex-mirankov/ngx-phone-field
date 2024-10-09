f00346c718d2e68a1440f00bc7890dbbcb0f6c05
stricozetc <alexandermirankov@gmail.com>
2024-10-10 00:00:21 +0400

    feat: rename the lib

--- projects/intl-tel-demo/src/app/app.component.ts
+++ projects/intl-tel-demo/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgxIntlPhoneField } from '../../../ngx-intl-phone-field/src/public-api';
+import { NgxPhoneField } from '../../../ngx-phone-field/src/public-api';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Iti } from 'intl-tel-input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, ReactiveFormsModule, NgxIntlPhoneField],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule, NgxPhoneField],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ projects/ngx-phone-field/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of intl-tel-input
+ */
+
+export * from './lib/ngx-phone-field.directive';
